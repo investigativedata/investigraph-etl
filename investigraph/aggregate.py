46cbcba55984f37339c95c1e2a79b1a036384a74
Simon Wörpel <simon.woerpel@pm.me>
2023-05-31 09:28:46 +0200

    ✨ (aggregate) add ftm store aggregation

--- investigraph/aggregate.py
+++ investigraph/aggregate.py
@@ -2,6 +2,12 @@
 aggregate fragments
 """
 
+from uuid import uuid4
+
+from ftmstore import get_dataset
+from ftmstore.cli import iterate_stream, write_stream
+from smart_open import open
+
 from investigraph.util import smart_iter_proxies, smart_write_proxies
 
 
@@ -16,3 +22,13 @@ def in_memory(in_uri: str, out_uri: str) -> tuple[int, int]:
             buffer[proxy.id] = proxy
     proxies = smart_write_proxies(out_uri, buffer.values(), serialize=True)
     return fragments, proxies
+
+
+def in_db(in_uri: str, out_uri: str) -> tuple[int, int]:
+    dataset = get_dataset("aggregate_%s" % uuid4().hex)
+    infile = open(in_uri)
+    outfile = open(out_uri)
+    write_stream(dataset, infile)
+    iterate_stream(dataset, outfile)
+    dataset.drop()
+    return 0, 0  # FIXME
