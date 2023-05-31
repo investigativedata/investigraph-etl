2b85b81e4466065b3ff87f3ecf9ac8a236f95fe5
Simon Wörpel <simon.woerpel@pm.me>
2023-05-31 09:56:18 +0200

    ✨ (extract) add csv option to pandas loading

--- investigraph/extract.py
+++ investigraph/extract.py
@@ -14,8 +14,14 @@ from investigraph.model import SourceResult
 def iter_records(res: SourceResult) -> Generator[dict, None, None]:
     if res.mimetype in (types.XLS, types.XLSX):
         df = pd.read_excel(BytesIO(res.content), **res.extract_kwargs).fillna("")
-
         for _, row in df.iterrows():
             yield dict(row)
         return
+
+    if res.mimetype == types.CSV:
+        df = pd.read_csv(BytesIO(res.content), **res.extract_kwargs).fillna("")
+        for _, row in df.iterrows():
+            yield dict(row)
+        return
+
     raise NotImplementedError("unsupported mimetype: `%s`" % res.mimetype)
