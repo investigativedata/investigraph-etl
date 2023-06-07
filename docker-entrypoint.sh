7175a9cd1a6a40d1bd56de9b5f1c316cd6361d5b
Simon Wörpel <simon.woerpel@pm.me>
2023-06-07 22:10:24 +0200

    📦️ (docker) add local datasets block

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,7 +5,9 @@
 if [ ! -f "/data/prefect/initialized" ]; then
     echo "initializing ..."
     echo `date +"%Y-%m-%d %H:%M:%S"` > /data/prefect/initialized
-    echo "adding default datasets block ..."
+    echo "adding local datasets block ..."
+    investigraph add-block -b local-file-system/datasets -u /data/local/datasets
+    echo "adding github datasets block ..."
     investigraph add-block -b github/investigraph-datasets -u https://github.com/investigativedata/investigraph-datasets.git
     echo "applying default deployment ..."
     prefect deployment build investigraph/pipeline.py:run -n investigraph-local --apply
