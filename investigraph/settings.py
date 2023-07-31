c3de8ad74c6466b3257baf7ae0a4024714bf8563
Simon Wörpel <simon.woerpel@pm.me>
2023-07-31 15:13:12 +0200

    🔖 Bump version: 0.3.0 → 0.3.1

--- investigraph/settings.py
+++ investigraph/settings.py
@@ -12,7 +12,7 @@ def get_env(env: str, default: Any | None = None) -> Any | None:
     return os.environ.get(env, default)
 
 
-VERSION = "0.3.0"
+VERSION = "0.3.1"
 
 DEBUG = as_bool(get_env("DEBUG", 1))
 DATA_ROOT = Path(get_env("DATA_ROOT", Path.cwd() / "data")).absolute()
