85cda573421aea108c35c67ba51f9350ba1c61f1
Simon Wörpel <simon.woerpel@pm.me>
2023-11-13 12:46:24 +0100

    👽️ (aggregate) use ftmq merge function

--- investigraph/logic/aggregate.py
+++ investigraph/logic/aggregate.py
@@ -5,7 +5,7 @@ aggregate fragments
 from typing import TYPE_CHECKING, Literal, TypeAlias
 from uuid import uuid4
 
-from followthemoney import model
+from ftmq.aggregate import merge
 from ftmq.io import smart_read_proxies
 from ftmq.model.coverage import Collector
 from ftmstore import get_dataset
@@ -15,31 +15,11 @@ if TYPE_CHECKING:
 
 from ftmq.model import Coverage
 
-from investigraph.types import CE, CEGenerator
+from investigraph.types import CEGenerator
 
 COMMON_SCHEMAS = ("Organization", "LegalEntity")
 
 AggregatorResult: TypeAlias = tuple[int, Coverage]
-
-
-def merge(ctx: "Context", p1: CE, p2: CE) -> CE:
-    try:
-        p1 = p1.merge(p2)
-        p1.schema = model.common_schema(p1.schema, p2.schema)
-        return p1
-    except Exception as e:
-        # try common schemata, this will probably "downgrade" entities
-        # as in, losing some schema specific properties
-        for schema in COMMON_SCHEMAS:
-            if p1.schema.is_a(schema) and p2.schema.is_a(schema):
-                p1 = ctx.make(schema, **p1.to_dict()["properties"])
-                p1.id = p2.id
-                p2 = ctx.make(schema, **p2.to_dict()["properties"])
-                p1 = p1.merge(p2)
-                return p1
-
-        ctx.log.warn(f"{e}, id: `{p1.id}`")
-        return p1
 
 
 class Aggregator:
@@ -75,7 +55,7 @@ class Aggregator:
         buffer = {}
         for proxy in self.get_fragments():
             if proxy.id in buffer:
-                buffer[proxy.id] = merge(self.ctx, buffer[proxy.id], proxy)
+                buffer[proxy.id] = merge(buffer[proxy.id], proxy)
             else:
                 buffer[proxy.id] = proxy
         yield from buffer.values()
